e7b07af9a369387af6dbd207d363571ed073bd9a
therealone989 <mrfabigm@gmail.com>
2024-01-23 15:33:30 +0100

    asd

---
+++ ticketsystem/src/test/java/com/projektwochejf/ticketsystem/TicketsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projektwochejf.ticketsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
